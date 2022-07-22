7318d88d90275f575ea3a6d1975fb74dfe8b41c7
tamojeetroychowdhury <102150252+tamojeetroychowdhury@users.noreply.github.com>
2022-07-22 09:33:03 +0530

    Add LISM files

--- Master-in-C/bubblesort.h
+++ Master-in-C/bubblesort.h
@@ -2,31 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 
-void bubbleSort2(double arr[][2], int n, int ind)
-{
-    int i, j;
-    long double temp[2];
-    for (i = 0; i < n; i++)
-    {
-        for (j = 0; j < n - i - 1; j++)
-        {
-            if (arr[j][ind] > arr[j + 1][ind])
-            {
-                // swap the elements
-                temp[0] = arr[j][0];
-                temp[1] = arr[j][1];
-
-                arr[j][0] = arr[j + 1][0];
-                arr[j][1] = arr[j + 1][1];
-
-                arr[j + 1][0] = temp[0];
-                arr[j + 1][1] = temp[1];
-            }
-        }
-    }
-}
-
-void bubbleSort3(double arr[][3], int n, int ind)
+void bubbleSort(long double arr[][3], int n)
 {
     int i, j;
     long double temp[3];
@@ -34,17 +10,15 @@ void bubbleSort3(double arr[][3], int n, int ind)
     {
         for (j = 0; j < n - i - 1; j++)
         {
-            if (arr[j][ind] > arr[j + 1][ind])
+            if (arr[j][1] * arr[j][1] + arr[j][2] * arr[j][2] > arr[j + 1][1] * arr[j + 1][1] + arr[j + 1][2] * arr[j + 1][2])
             {
                 // swap the elements
                 temp[0] = arr[j][0];
                 temp[1] = arr[j][1];
                 temp[2] = arr[j][2];
-
                 arr[j][0] = arr[j + 1][0];
                 arr[j][1] = arr[j + 1][1];
                 arr[j][2] = arr[j + 1][2];
-
                 arr[j + 1][0] = temp[0];
                 arr[j + 1][1] = temp[1];
                 arr[j + 1][2] = temp[2];
---
+++ Master-in-C/sm_constants.h
@@ -0,0 +1,5 @@
+#define FOCAL_LENGTH 0.036
+#define EPSILON 2.22e-15
+#define DELTA 1e-6
+#define ANG_DIST_TOLERANCE 1.2
+#define VOTE_TOLERANCE 0.5
